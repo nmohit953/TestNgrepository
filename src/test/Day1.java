15bd349839bfafd7c363247b87c5e9458044f6aa
HP <HP@DESKTOP-G6OMV75>
2024-06-22 16:22:43 +0530

    commit 1

---
+++ src/test/Day1.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+	
+	@Test
+	public void personalLoan() {
+		System.out.println("personalLoan");
+	}
+	@Test
+	public void goldLoan() {
+		System.out.println("goldLoan");
+	}
+	@Test
+	public void carLoan() {
+		System.out.println("carLoan");
+	}
+	
+
+}
---
+++ src/test/Day2.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+	@Test
+	public void personalLoan() {
+		System.out.println("personalLoan2");
+	}
+	@Test
+	public void goldLoan() {
+		System.out.println("goldLoan2");
+	}
+	@Test
+	public void carLoan() {
+		System.out.println("carLoan2");
+	}
+
+}
