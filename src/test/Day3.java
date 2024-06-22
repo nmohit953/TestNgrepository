04ee9018ddac259e108e2fc2945cef8eb030b001
HP <HP@DESKTOP-G6OMV75>
2024-06-22 17:01:01 +0530

    Day3 update

--- src/test/Day3.java
+++ src/test/Day3.java
@@ -1,5 +1,19 @@
 package test;
 
+import org.testng.annotations.Test;
+
 public class Day3 {
+	@Test
+	public void personalLoan() {
+		System.out.println("personalLoan3");
+	}
+	@Test
+	public void goldLoan() {
+		System.out.println("goldLoan3");
+	}
+	@Test
+	public void carLoan() {
+		System.out.println("carLoan3");
+	}
 
 }
